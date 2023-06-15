d837decd7de779cd2215dab7b614a624e89d0f6f
bobbyrabia <rpfreire1@espe.edu.ec>
2023-06-14 21:16:47 -0500

    Servicio y Rest de Sede

--- src/main/java/ec/espe/edu/arquitectura/examen_roberto_freire/controller/SedeController.java
+++ src/main/java/ec/espe/edu/arquitectura/examen_roberto_freire/controller/SedeController.java
@@ -3,10 +3,7 @@ package ec.espe.edu.arquitectura.examen_roberto_freire.controller;
 import ec.espe.edu.arquitectura.examen_roberto_freire.model.Sede;
 import ec.espe.edu.arquitectura.examen_roberto_freire.service.SedeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +19,20 @@ public class SedeController {
     @GetMapping("/list-sede/{institucionId}")
     public ResponseEntity<List<Sede>> encontrarSedesPorInstitucion(
             @PathVariable(name = "institucionId") Integer institucionId
-    ){
-        List<Sede> sedeList=this.sedeService.encontrarSedesPorInstitucion(institucionId);
+    ) {
+        List<Sede> sedeList = this.sedeService.encontrarSedesPorInstitucion(institucionId);
         return ResponseEntity.ok(sedeList);
+    }
+
+    @PostMapping(value = "/create-sede")
+    public ResponseEntity<Sede> createSede(@RequestBody Sede sede) {
+        try {
+            Sede sedeTemp=this.sedeService.createSede(sede);
+            return ResponseEntity.ok(sedeTemp);
+        } catch (RuntimeException rte) {
+            return ResponseEntity.badRequest().build();
+        }
+
+
     }
 }
--- src/main/java/ec/espe/edu/arquitectura/examen_roberto_freire/service/EdificioService.java
+++ src/main/java/ec/espe/edu/arquitectura/examen_roberto_freire/service/EdificioService.java
@@ -1,4 +1,17 @@
 package ec.espe.edu.arquitectura.examen_roberto_freire.service;
 
+import ec.espe.edu.arquitectura.examen_roberto_freire.repository.EdificioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EdificioService {
+    private final EdificioRepository edificioRepository;
+
+    public EdificioService(EdificioRepository edificioRepository) {
+        this.edificioRepository = edificioRepository;
+    }
+
+
+
+
 }
